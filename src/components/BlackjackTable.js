14e87072455c5e987c2b8e5ae2058490f085099c
Max <max@maxg.dev>
2021-09-03 16:27:52 +0100

    Add player colours.

--- src/components/BlackjackTable.js
+++ src/components/BlackjackTable.js
@@ -2,15 +2,17 @@ import styles from './BlackjackTable.module.css';
 
 export default function BlackjackTable({ players, winner }) {
 
+    const cls = (name) => name === "Dealer" ? styles.dealer : styles.player;
+
     return (
         <main className={styles.main}>
             <div>
                 <hr />
                 {players.length > 0 ?
-                    players.map(p => <div className={styles.card}>{p.name + ":" + p.cards.map(c => " " + c.cardType)}</div>)                  
+                    players.map(p => <div className={styles.card + " " + cls(p.name)}>{p.name + ":" + p.cards.map(c => " " + c.cardType)}</div>)                  
                     : "Click the button above to get new cards."}
                 <hr />
-                {winner && <div>Winner: {winner.name}</div>}
+                {winner && <div className={cls(winner.name)}>Winner: {winner.name}</div>}
             </div>
         </main>
     )
